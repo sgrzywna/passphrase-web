64c755cfb09ffcb451da3be542ec6b18aa5ec698
Sylwester Grzywna <sgrzywna@gmail.com>
2019-01-06 18:50:44 +0100

    First working release.

--- main.go
+++ main.go
@@ -4,14 +4,10 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"net/url"
-	"path/filepath"
-	"sort"
 	"strconv"
-	"strings"
 
 	rice "github.com/GeertJohan/go.rice"
 )
@@ -29,63 +25,47 @@ func main() {
 	flag.Parse()
 	log.Printf("Listening @ :%d...", *port)
 
-	tmplBox, err := rice.FindBox("web/template")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	indexTmpl, err := tmplBox.String("index.tmpl")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	t, err := template.New("index.html").Parse(indexTmpl)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	generator, err := NewGenerator(*dictDir)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	staticBox, err := rice.FindBox("web/static")
+	distBox, err := rice.FindBox("web/dist")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(staticBox.HTTPBox())))
-	http.HandleFunc("/passwords.json", limit(getPasswords(generator)))
-	http.HandleFunc("/", getIndexHandler(generator, t))
+	http.Handle("/", http.FileServer(distBox.HTTPBox()))
+	http.HandleFunc("/api/passwords.json", limit(getPasswords(generator)))
 
 	initLimiter()
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
 
-func getIndexHandler(generator *Generator, t *template.Template) http.HandlerFunc {
-	type selectData struct {
-		Name  string
-		Value string
-	}
+// func getIndexHandler(generator *Generator, t *template.Template) http.HandlerFunc {
+// 	type selectData struct {
+// 		Name  string
+// 		Value string
+// 	}
 
-	var dictNames []selectData
+// 	var dictNames []selectData
 
-	files := generator.GetDictFiles()
-	sort.Strings(files)
+// 	files := generator.GetDictFiles()
+// 	sort.Strings(files)
 
-	for _, file := range files {
-		name := strings.Title(strings.TrimSuffix(file, filepath.Ext(file)))
-		dictNames = append(dictNames, selectData{name, file})
-	}
+// 	for _, file := range files {
+// 		name := strings.Title(strings.TrimSuffix(file, filepath.Ext(file)))
+// 		dictNames = append(dictNames, selectData{name, file})
+// 	}
 
-	return func(w http.ResponseWriter, r *http.Request) {
-		err := t.Execute(w, dictNames)
-		if err != nil {
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		}
-	}
-}
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		err := t.Execute(w, dictNames)
+// 		if err != nil {
+// 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+// 		}
+// 	}
+// }
 
 func getPasswords(generator *Generator) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
