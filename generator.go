b1b112c4e07d7c5249d4337895bed6ac83249d45
Sylwester Grzywna <sylwester.grzywna@gmail.com>
2019-01-26 09:59:02 +0100

    Minor code refactoring.

--- generator.go
+++ generator.go
@@ -1,24 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"strings"
 )
 
 const (
-	dictFileSuffix = ".dict"
+	dictFileSuffix           = ".dict"
+	maxPasswordsCount int    = 128
+	maxWordsCount     int    = 16
+	separators        string = " +-=:.,!#%&*_"
 )
 
 // Generator represents password generator.
 type Generator struct {
-	dicts map[string][]string
+	dicts     map[string][]string
+	randIndex *RandIndex
 }
 
 // NewGenerator returns initialized Generator object.
 func NewGenerator(dictsDir string) (*Generator, error) {
 	generator := Generator{
-		dicts: make(map[string][]string),
+		dicts:     make(map[string][]string),
+		randIndex: NewRandIndex(),
 	}
 	if err := generator.loadDicts(dictsDir); err != nil {
 		return nil, err
@@ -41,7 +48,7 @@ func (g *Generator) GeneratePasswords(dict string, passwords, words int) []strin
 	if !ok {
 		return []string{}
 	}
-	return GeneratePasswords(dictionary, passwords, words)
+	return g.getPasswords(dictionary, passwords, words)
 }
 
 // loadDicts loads dictionary files into memory.
@@ -54,11 +61,70 @@ func (g *Generator) loadDicts(dictsDir string) error {
 		if file.IsDir() || !strings.HasSuffix(file.Name(), dictFileSuffix) {
 			continue
 		}
-		words, err := LoadDictionary(filepath.Join(dictsDir, file.Name()))
+		words, err := g.loadDictionary(filepath.Join(dictsDir, file.Name()))
 		if err != nil {
 			return err
 		}
 		g.dicts[strings.ToLower(file.Name())] = words
 	}
 	return nil
+}
+
+// GeneratePasswordsFromFile returns list of random passwords generated from dictionary stored in file.
+func (g *Generator) generatePasswordsFromFile(dictionary string, passwordsCount int, wordsCount int) ([]string, error) {
+	d, err := g.loadDictionary(dictionary)
+	if err != nil {
+		return nil, err
+	}
+	passwords := g.getPasswords(d, passwordsCount, wordsCount)
+	return passwords, nil
+}
+
+// getPasswords returns list of random passwords generated from dictionary stored in memory.
+func (g *Generator) getPasswords(dictionary []string, passwordsCount int, wordsCount int) []string {
+	if len(dictionary) == 0 {
+		return []string{}
+	}
+
+	if passwordsCount < 1 {
+		passwordsCount = 1
+	} else if passwordsCount > maxPasswordsCount {
+		passwordsCount = maxPasswordsCount
+	}
+
+	if wordsCount < 1 {
+		wordsCount = 1
+	} else if wordsCount > maxWordsCount {
+		wordsCount = maxWordsCount
+	}
+
+	var passwords []string
+	maxNumber := uint32(len(dictionary) - 1)
+
+	for i := 0; i < passwordsCount; i++ {
+		var words []string
+		for j := 0; j < wordsCount; j++ {
+			ndx := g.randIndex.RandInt32(maxNumber)
+			words = append(words, dictionary[ndx])
+		}
+		sep := separators[g.randIndex.RandInt32(uint32(len(separators))-1)]
+		passwords = append(passwords, strings.Join(words, string(sep)))
+	}
+
+	return passwords
+}
+
+// loadDictionary loads dictionary words into memory.
+func (g *Generator) loadDictionary(dictionary string) ([]string, error) {
+	file, err := os.Open(dictionary)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
--- generator_test.go
+++ generator_test.go
@@ -8,6 +8,11 @@ import (
 	"testing"
 )
 
+var words = []string{
+	"1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+	"22", "333", "4444", "55555", "666666", "7777777", "88888888", "999999999",
+}
+
 func TestGetDictFiles(t *testing.T) {
 	names := []string{"first.dict", "second.dict", "third.dict"}
 	dictsFolder := getFolderWithDicts(t, words, names...)
@@ -24,33 +29,132 @@ func TestGetDictFiles(t *testing.T) {
 	}
 }
 
-// func TestLoadDicts(t *testing.T) {
-// 	names := []string{"first.dict", "second.dict", "third.dict"}
-// 	dictsFolder := getFolderWithDicts(t, words, names...)
-// 	defer os.RemoveAll(dictsFolder)
-// 	dicts, err := loadDicts(dictsFolder)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	for _, dict := range dicts {
-// 		if !findWord(names, dict.fileName) {
-// 			t.Errorf("unexpected dict name %s", dict.fileName)
-// 		}
-// 		for _, word := range dict.words {
-// 			if !findWord(words, word) {
-// 				t.Errorf("unexpected word %s", word)
-// 			}
-// 		}
-// 	}
-// }
+func TestGeneratePasswords(t *testing.T) {
+	dictsFolder := getDictsFolder(t)
+	defer os.RemoveAll(dictsFolder)
+
+	g, err := NewGenerator(dictsFolder)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	passwordsCount := 3
+	wordsCount := 3
+
+	passwords := g.getPasswords(words, passwordsCount, wordsCount)
+	if len(passwords) != int(passwordsCount) {
+		t.Fatalf("expected %d passwords, got %d", passwordsCount, len(passwords))
+	}
+
+	for _, p := range passwords {
+		tokens := strings.FieldsFunc(p, func(r rune) bool {
+			return strings.ContainsRune(separators, r)
+		})
+		if len(tokens) != wordsCount {
+			t.Fatalf("expected %d words, got %d (%s)", wordsCount, len(tokens), p)
+		}
+		for _, token := range tokens {
+			if !findWord(words, token) {
+				t.Fatalf("uexpected word %s", token)
+			}
+		}
+	}
+}
+
+func TestGeneratePasswordsOnlyOne(t *testing.T) {
+	dictsFolder := getDictsFolder(t)
+	defer os.RemoveAll(dictsFolder)
+
+	g, err := NewGenerator(dictsFolder)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	passwordsCount := 1
+	wordsCount := 1
+
+	passwords := g.getPasswords(words, passwordsCount, wordsCount)
+	if len(passwords) != int(passwordsCount) {
+		t.Fatalf("expected %d passwords, got %d", passwordsCount, len(passwords))
+	}
+
+	for _, p := range passwords {
+		tokens := strings.FieldsFunc(p, func(r rune) bool {
+			return strings.ContainsRune(separators, r)
+		})
+		if len(tokens) != wordsCount {
+			t.Fatalf("expected %d words, got %d (%s)", wordsCount, len(tokens), p)
+		}
+		for _, token := range tokens {
+			if !findWord(words, token) {
+				t.Fatalf("unexpected word %s", token)
+			}
+		}
+	}
+}
+
+func TestGeneratePasswordsFromEmptyList(t *testing.T) {
+	dictsFolder := getDictsFolder(t)
+	defer os.RemoveAll(dictsFolder)
+
+	g, err := NewGenerator(dictsFolder)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	words := []string{}
+	passwordsCount := 3
+	wordsCount := 3
+
+	passwords := g.getPasswords(words, passwordsCount, wordsCount)
+	if len(passwords) != 0 {
+		t.Fatalf("expected 0 passwords, got %d", len(passwords))
+	}
+}
+
+func TestGeneratePasswordsZero(t *testing.T) {
+	dictsFolder := getDictsFolder(t)
+	defer os.RemoveAll(dictsFolder)
+
+	g, err := NewGenerator(dictsFolder)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	words := []string{}
+	passwordsCount := 0
+	wordsCount := 0
+
+	passwords := g.getPasswords(words, passwordsCount, wordsCount)
+	if len(passwords) != 0 {
+		t.Fatalf("expected 0 passwords, got %d", len(passwords))
+	}
+}
+
+func findWord(words []string, word string) bool {
+	for _, w := range words {
+		if word == w {
+			return true
+		}
+	}
+	return false
+}
 
 func getFolderWithDicts(t *testing.T, words []string, names ...string) string {
 	data := strings.Join(words, "\n")
-	tmpDir := os.TempDir()
+	tmpDir := getDictsFolder(t)
 	for _, name := range names {
 		if err := ioutil.WriteFile(filepath.Join(tmpDir, name), []byte(data), os.ModePerm); err != nil {
 			t.Fatal(err)
 		}
+	}
+	return tmpDir
+}
+
+func getDictsFolder(t *testing.T) string {
+	tmpDir := filepath.Join(os.TempDir(), "dicts")
+	if err := os.MkdirAll(tmpDir, os.ModePerm); err != nil {
+		t.Fatal(err)
 	}
 	return tmpDir
 }
